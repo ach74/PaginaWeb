73b5e51e9d81e15aa3f4a5d81b9ab924212d4256
Ach74 <achraf074@gmail.com>
2018-01-25 20:00:25 +0100

    ach
     archoivo XML

--- js/js.js
+++ js/js.js
@@ -4,25 +4,25 @@
 
 window.onload = function cargar(){
 	document.getElementById("Opc1").onclick=function(){Opc1();};
-	document.getElementByID("Opc2").onclick=function(){Opc2();}; 
-	document.getElementByID("Opc3").onclick=function(){Opc3();}; 
+	document.getElementById("Opc2").onclick=function(){Opc2();};
+	document.getElementById("Opc3").onclick=function(){Opc3();};
 
 }
 
 function Opc1(){
-	document.getElementById("Opc1").style.display="block";
-	document.getElementByID("Opc2").style.display="none";
-	document.getElementByID("Opc3").style.display="none";
+	document.getElementById("Informacion").style.display="block";
+	document.getElementById("Ubicacion").style.display="none";
+	document.getElementById("Test").style.display="none";
 }
 
 function Opc2(){
-	document.getElementByID("Opc2").style.display="block";
-	document.getElementByID("Opc1").style.display="none";
-	document.getElementByID("Opc3").style.display="none";
+	document.getElementById("Ubicacion").style.display="block";
+	document.getElementById("Informacion").style.display="none";
+	document.getElementById("Test").style.display="none";
 }
 
 function Opc3(){
-	document.getElementByID("Opc3").style.display="block";
-	document.getElementByID("Opc1").style.display="none";
-	document.getElementByID("Opc3").style.display="none";
+	document.getElementById("Test").style.display="block";
+	document.getElementById("Informacion").style.display="none";
+	document.getElementById("Ubicacion").style.display="none";
 }
