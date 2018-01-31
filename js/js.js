9a0505e367bb89ff09701d15ae17936f6d20636e
Ach74 <achraf074@gmail.com>
2018-01-31 20:58:32 +0100

    ach
    Corregir errores de orden

--- js/js.js
+++ js/js.js
@@ -5,6 +5,7 @@ var xhttp = new XMLHttpRequest();
 
 
 
+
 window.onload = function cargar(){
 	document.getElementById("Opc1").onclick=function(){Opc1();};
 	document.getElementById("Opc2").onclick=function(){Opc2();};
@@ -15,17 +16,18 @@ window.onload = function cargar(){
 function Opc1(){
 	document.getElementById("Informacion").style.display="block";
 	document.getElementById("Ubicacion").style.display="none";
-	document.getElementById("Test").style.display="none";
+	document.getElementById("myform").style.display="none";
 }
 
 function Opc2(){
 	document.getElementById("Ubicacion").style.display="block";
 	document.getElementById("Informacion").style.display="none";
-	document.getElementById("Test").style.display="none";
+	document.getElementById("myform").style.display="none";
+
 }
 
 function Opc3(){
-	document.getElementById("Test").style.display="block";
+	document.getElementById("myform").style.display="block";
 	document.getElementById("Informacion").style.display="none";
 	document.getElementById("Ubicacion").style.display="none";
 }
@@ -42,16 +44,16 @@ xhttp.send();
 // función personalizada que gestiona la respuesta a la petición de fichero
 function gestionarXml(dadesXml){
   var xmlDoc = dadesXml.responseXML;
-  document.getElementById("p1").innerHTML = xmlDoc.getElementsByTagName("title")[0].innerHTML;
-  document.getElementById("r1").innerHTML = xmlDoc.getElementsByTagName("answer")[0].innerHTML;
 
-  document.getElementById("p2").innerHTML = xmlDoc.getElementsByTagName("title")[1].innerHTML;
-  document.getElementById("r2").innerHTML = xmlDoc.getElementsByTagName("answer")[1].innerHTML;
 
-  document.getElementById("p3").innerHTML = xmlDoc.getElementsByTagName("title")[2].innerHTML;
-  document.getElementById("r3").innerHTML = xmlDoc.getElementsByTagName("answer")[2].innerHTML;
-  document.getElementById("op1").innerHTML = xmlDoc.getElementsByTagName("option")[0].innerHTML;
-  document.getElementById("op2").innerHTML = xmlDoc.getElementsByTagName("option")[0].innerHTML;
-  document.getElementById("op3").innerHTML = xmlDoc.getElementsByTagName("option")[0].innerHTML;
+	var p;
+	var op;
+  
+for (var i = 0 ; i <= 10; i++) {
 
+
+		p = "p" + (i + 1) ;
+		document.getElementById(p).innerHTML = xmlDoc.getElementsByTagName("title")[i].innerHTML;
+	}
 }
+
