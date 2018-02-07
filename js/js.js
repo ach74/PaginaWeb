254352262a587bf21f771acd98c98ec6310b32dd
Ach74 <achraf074@gmail.com>
2018-02-07 20:59:01 +0100

    ach

--- js/js.js
+++ js/js.js
@@ -2,15 +2,31 @@
 
 var url ="https://cdn.rawgit.com/ach74/PaginaWeb/a3047ad5/xml/xml.xml";
 var xhttp = new XMLHttpRequest();
-var Numerico= null;
 
 
 
 
-window.onload = function cargar(){
-	document.getElementById("Opc1").onclick=function(){Opc1();};
-	document.getElementById("Opc2").onclick=function(){Opc2();};
-	document.getElementById("Opc3").onclick=function(){Opc3();};
+
+window.onload = function(){
+	document.getElementById("Opc1").onclick=function(){
+		Opc1();
+	}
+	document.getElementById("Opc2").onclick=function(){
+		Opc2();
+	}
+	document.getElementById("Opc3").onclick=function(){
+		Opc3();
+	}
+
+	 xhttp.onreadystatechange = function() {
+   if (this.readyState == 4 && this.status == 200) {
+    // función personalizada que gestiona la respuesta a la petición de fichero
+    gestionarXml(this); 
+   }
+  };
+  xhttp.open("GET", url, true); //url del fichero
+  xhttp.send();
+
 
 }
 
@@ -33,36 +49,33 @@ function Opc3(){
 	document.getElementById("Ubicacion").style.display="none";
 }
 
-xhttp.onreadystatechange = function() {
- if (this.readyState == 4 && this.status == 200) {
-  // función personalizada que gestiona la respuesta a la petición de fichero
-  gestionarXml(this); 
- }
-};
-xhttp.open("GET", url, true); //url del fichero
-xhttp.send();
+function impSelect(s, i){
+	var select = document.getElementsByTagName("select")[i-2];
+	for( i = 0; i < s.length; i ++){
+		var opc = document.createElement("option");
+		opc.text = s[i];
+		opc.value = i + 1;
+		select.options.add(option);
+	}
+}
 
 // función personalizada que gestiona la respuesta a la petición de fichero
 function gestionarXml(dadesXml){
   var xmlDoc = dadesXml.responseXML;
 
-		var p; 
-		var r;
-	for (var i = 0 ; i <= 10; i++) {
-			p = "p" + (i + 1) ;
-			document.getElementById(p).innerHTML = xmlDoc.getElementsByTagName("title")[i].innerHTML;
-				if (i <= 1){
-				r = "r" + (i+1)
-				document.getElementById(r).innerHTML =parseInt(xmlDoc.getElementsByTagName("answer")[i].innerHTML);
-				}else if (i >= 2 && i <= 3) {
-					var opcionesSelect = [];
-					var prg = "pregunta" + i;
- 					var nopt = xmlDoc.getElementById(prg).getElementsByTagName('option')[i].innerHTML;
-						
-				}
 
-
+	for (i = 0 ; i <= 10; i++) {
+			
+			document.getElementsByTagName("span")[i].innerHTML = xmlDoc.getElementsByTagName("title")[i].innerHTML;
+	}
+	//Leer Selector multiple
+	for(i = 2 ; i < 4; i ++){
+		var s = [];
+		var Max = xmlDoc.getElementsByTagName("question")[i].getElementsByTagName('option').length;
+		for(j = 0; j < Max; j++){
+			s[j] = xmlDoc.getElementsByTagName("question")[i].getElementsByTagName("option")[j].innerHTML;
 		}
-
+		impSelect(s, i);
+	}
 }
 
